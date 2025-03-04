3088f1cf63a983030715a87483f434aaeb769a15
Azwar-as <azwaranshari08@gmail.com>
2025-03-04 18:32:54 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+const inputNumber = document.getElementById("inputNumber");
+const fromConv = document.getElementById("fromConv");
+const toConv = document.getElementById("toConv");
+
+document.getElementById("btnChange").addEventListener("click", () => {
+  const fromResult = document.getElementById("fromResult");
+  const toResult = document.getElementById("toResult");
+  const convInfo = document.getElementById("convertInfo");
+
+  const temp = fromConv.value;
+  fromConv.value = toConv.value;
+  toConv.value = temp;
+
+  fromResult.textContent = `From (${fromConv.value}) = 0`;
+  toResult.textContent = `0 ${toConv.value}`;
+  convInfo.textContent = `Converted From ${fromConv.value} to ${toConv.value}`;
+});
+
+document.getElementById("btnCalculate").addEventListener("click", () => {
+  const fromResult = document.getElementById("fromResult");
+  const toResult = document.getElementById("toResult");
+  const convInfo = document.getElementById("convertInfo");
+
+  const number = inputNumber.value;
+  const fromBase = getBase(fromConv.value);
+  const toBase = getBase(toConv.value);
+
+  if (number && fromBase && toBase) {
+    const convertedNumber = convertNumber(number, fromBase, toBase);
+    fromResult.textContent = `From ${fromConv.value} = ${number}`;
+    toResult.textContent = `${convertedNumber} ${toConv.value}`;
+    convInfo.textContent = `Converted From ${fromConv.value} to ${toConv.value}`;
+  } else {
+    convInfo.textContent =
+      "Please enter a valid number and select conversion types.";
+  }
+});
+
+function getBase(type) {
+  switch (type) {
+    case "biner":
+      return 2;
+    case "oktal":
+      return 8;
+    case "desimal":
+      return 10;
+    case "hexa":
+      return 16;
+    default:
+      return null;
+  }
+}
+
+function convertNumber(number, fromBase, toBase) {
+  const decimalNumber = parseInt(number, fromBase);
+  return decimalNumber.toString(toBase);
+}
